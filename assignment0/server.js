3b9de8e30ff66964aedd1d933d2b84475096c630
unknown <JING2128@hotaimotor.com.tw>
2023-05-02 09:43:11 +0800

    deploy plant.html

--- assignment0/server.js
+++ assignment0/server.js
@@ -3,7 +3,8 @@ var express = require('express');
 var fs = require('fs');
 var app = express();
 var server = http.createServer(app);
-var content = fs.readFileSync('plant.html');
+var plant = fs.readFileSync('plant.html');
+var plantShop = fs.readFileSync('plantShop.html');
 
 
 app.use('/css', express.static('css'));
@@ -11,7 +12,13 @@ app.use('/img', express.static('img'));
 
 app.get('/', function(request,response){
   response.writeHeader(200,{'Content-Type':'text/html'});
-  response.write(content);
+  response.write(plant);
+  response.end();
+});
+
+app.get('/plantShop', function(request,response){
+  response.writeHeader(200,{'Content-Type':'text/html'});
+  response.write(plantShop);
   response.end();
 });
 
