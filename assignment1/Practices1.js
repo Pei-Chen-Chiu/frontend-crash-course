d08545c4a15b2e61ff581ac9a50ede5f9f8c5ae8
unknown <JING2128@hotaimotor.com.tw>
2023-05-08 14:59:21 +0800

    test

--- assignment1/Practices1.js
+++ assignment1/Practices1.js
@@ -1,4 +1,53 @@
+// Declare two variables and assign them with numerical values
+const x = 10;
+const y = 5;
+
+
 // 1. Declare some variable and log the result of variables arithmetic.
+console.log(`${'-'.repeat(8)}arithmetic${'-'.repeat(8)}`);
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+
+
 // 2. Declare some variable and assign the result of variables arithmetic to another variable and log it.
+console.log(`${'-'.repeat(8)}arithmetic${'-'.repeat(8)}`);
+let sum=x+y;
+let difference = x - y;
+let product = x * y;
+let quotient = x / y;
+
+
+console.log(sum);
+console.log(difference);
+console.log(product);
+console.log(quotient);
+
+
 // 3. Declare some variable and log the result to variables comparison.
+console.log(`${'-'.repeat(8)}comparison${'-'.repeat(8)}`);
+
+
+console.log(x < y);
+console.log(x > y);
+console.log(x == y);
+console.log(x != y);
+
+
 // 4. Declare some variable and log the result of variables logical convertion.
+console.log(`${'-'.repeat(8)}logical convertion${'-'.repeat(8)}`);
+
+let a = true;
+console.log(a);
+
+let b = !a;
+console.log(b);
+
+let c = true;
+let d = false;
+console.log(c && d);
+
+let e = true;
+let f = false;
+console.log(e || f);
--- assignment1/Practices2.js
+++ assignment1/Practices2.js
@@ -1,6 +1,92 @@
 // 1. Declare a variable called age and show ‘can vote’ if age >= 18, show ‘can’t vote’ if age < 18
+// let age = parseInt(prompt('Please enter your age:'), 10);
+let age = 20;
+
+if (age >= 18) {
+    console.log('can vote');
+} else {
+    console.log('can\'t vote');
+}
+
 // 2. Check if a number is odd or even.
+// let num = parseInt(prompt('Please enter a number:'), 10);
+let num = 1;
+// let num=12;
+
+if (num % 2 === 0) {
+    console.log(num + ' is even.');
+} else {
+    console.log(num + ' is odd.');
+}
+
 // 3. Declare two variables and check which one is big or they’re equaled.
+// let x = parseInt(prompt('Please enter the value of x:'), 10);
+// let y = parseInt(prompt('Please enter the value of y:'), 10);
+
+// let x = 5;
+// let y = 10;
+
+// let x = 10;
+// let y = 5;
+
+let x = 5;
+let y = 5;
+
+if (x > y) {
+    console.log('x is bigger than y');
+} else if (x < y) {
+    console.log('y is bigger than x');
+} else {
+    console.log('x and y are equal');
+}
+
+
 // 4. Declare three variables and check which one is big or they’re equaled.
+// let a = parseInt(prompt('Enter the value of a:'));
+// let b = parseInt(prompt('Enter the value of b:'));
+// let c = parseInt(prompt('Enter the value of c:'));
+
+let a = 2;
+let b = 2;
+let c = 2;
+
+
+
+if (a === b && b === c) {
+    console.log('a, b and c are equal')
+} else if (a === c && c > b) {
+    console.log('a and c are both the biggest')
+} else if (b === c && c > a) {
+    console.log('b and c are both the biggest')
+} else if (a === b && b > c) {
+    console.log('a and b are both the biggest')
+} else if (a > b && a > c) {
+    console.log('a is the biggest');
+} else if (b > a && b > c) {
+    console.log('b is the biggest');
+} else if (c > a && c > b) {
+    console.log('c is the biggest');
+}
+
+
 // 5. Declare two variables for range (ex. 10, 100), declare another number variable and check a number is present in given range
+let min = 10;
+let max = 100;
+let no = 10;
+
+if (no >= min && no <= max) {
+    console.log(`${no} is in the range of ${min} and ${max}`);
+} else {
+    console.log(`${no} is not in the range of ${min} and ${max}`);
+}
+
 // 6. Declare a variable called year and check the year is leap year or not.
+
+let year =2024;
+// 是 4 非 100 的倍數 閏年
+// 是 100 非 400 倍數 平年
+if (year % 4 === 0 && (year%100===0&&!year%400)) {
+    console.log(`${year} is a leap year`);
+} else {
+    console.log(`${year} is not a leap year`);
+}
